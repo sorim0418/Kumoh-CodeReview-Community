713a412815b343559026f9f34d5959eef01362e6
Jihyun3478 <leejihyun3478@gmail.com>
2023-10-30 14:43:04 +0900

    feat: 기본 세팅

---
+++ src/main/java/com/kcr/KcrApplication.java
@@ -0,0 +1,13 @@
+package com.kcr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KcrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kcr/KcrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KcrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
